429147a76d9632feeb4645676767bc5f56c323ad
Robert Dam <info@rd-media.org>
2025-03-05 12:01:02 +0100

    Update Filesystem to make it work locally

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,7 +60,7 @@ return [
 
 		'public' => [
 			'driver'     => 'local',
-			'root'       => storage_path('app/public'),
+			'root'       => storage_path('app'),
 			'url'        => env('APP_URL') . '/storage',
 			'visibility' => 'public',
 			'throw'      => false,
@@ -87,11 +87,11 @@ return [
 		],
 
 		'vultr' => [
-			'driver' => 's3',
-			'key'=> env('VULTR_ACCESS_KEY'),
-			'secret' => env('VULTR_SECRET_KEY'),
-			'region' => env('VULTR_REGION'),
-			'bucket' => env('VULTR_BUCKET'),
+			'driver'   => 's3',
+			'key'      => env('VULTR_ACCESS_KEY'),
+			'secret'   => env('VULTR_SECRET_KEY'),
+			'region'   => env('VULTR_REGION'),
+			'bucket'   => env('VULTR_BUCKET'),
 			'endpoint' => env('VULTR_ENDPOINT'),
 		],
 
